467b827d2ca58df2c8a33335097d5772fa9a6530
Parth Patel <parthkumar111patel@gmail.com>
2023-12-31 09:44:04 +0530

    Fix: auto reload on Menu Tab Change

--- client/src/App.js
+++ client/src/App.js
@@ -13,22 +13,21 @@ export const App = () => {
     return (
         <>
             <div className='container'>
-                <div className='bf4__bg' />
                 <Routes>
                     <Route path='/' element={<BattlefieldPage handleQuit={handleQuit} />} />
                     {/* <Route path='/store' element={<BattleFieldStore />} /> */}
                     <Route path='/battlepacks' element={<BattlePacks />} />
                 </Routes>
 
-                <div className='quit__wrapper' style={{display: `${quit ? 'flex' : 'none'}`}}>
-                    <div className='quit__options-w'>
-                        <div className='quit__text-w'>
-                            <div className='quit__text'>quit</div>
-                            <div className='quit__text-2'>Do you really want to quit?</div>
+                <div className='quit-w' style={{display: `${quit ? 'flex' : 'none'}`}}>
+                    <div className='quit-options'>
+                        <div className='quit-text-w'>
+                            <div className='quit-text'>quit</div>
+                            <div className='quit-text-2'>Do you really want to quit?</div>
                         </div>
-                        <div className='quit__buttons-w'>
-                            <div className='quit__button' onClick={() => setQuit(false)}>yes</div>
-                            <div className='quit__button quit__button--nomargin' onClick={() => setQuit(false)}>no</div>
+                        <div className='quit-buttons'>
+                            <div className='quit-button' onClick={() => setQuit(false)}>yes</div>
+                            <div className='quit-button quit-button-right' onClick={() => setQuit(false)}>no</div>
                         </div>
                     </div>
                 </div>
--- client/src/Battlefield4/BattlefieldPage.jsx
+++ client/src/Battlefield4/BattlefieldPage.jsx
@@ -6,10 +6,38 @@ import B4Level from '../Components/B4Level'
 import Sidemenu from '../Components/SideMenu'
 import Invite from '../Components/Invite'
 import BattleFieldStore from './BattleFieldStore'
+import { Link } from 'react-router-dom'
 
+const BattleFieldSoldier = () => {
+    return (
+        <div className='b4-content'>
+            <div className='soldier__content'>
+                <div className="soldier__look">
+                    <div className='soldier__dogtag'>
+                        <img
+                            src="https://uploads-ssl.webflow.com/6013fff62154adaa4600f932/6038fcc400244d4c8d9870cd_premium%20dog%20tag.png"
+                            className='soldier__dogtag-left'
+                        />
+                        <img
+                            src="https://uploads-ssl.webflow.com/6013fff62154adaa4600f932/6038fcc4c0c4fca91b669847_BF4_Celebration_Dog_Tag.png"
+                            className='soldier__dogtag-right'
+                        />
+                    </div>
+                    <img
+                        src="https://uploads-ssl.webflow.com/6013fff62154adaa4600f932/6038fcc5c0c4fc008966984e_soldier__pic.png"
+                        className='soldier__pic'
+                    />
+                </div>
+
+                <SoldierContent />
+            </div>
+        </div>
+    )
+}
 
 const BattlefieldPage = (props) => {
-    const [navMenuIdx, setNavMenuIdx] = useState(1);
+    const [navMenuIdx, setNavMenuIdx] = useState(0);
+
     return (
         <>
             <img src='https://uploads-ssl.webflow.com/6013fff62154adaa4600f932/6038fcc47b02ee10043f39c3_soldier__BFemblem.png' className='soldier__bfemblem' />
@@ -17,57 +45,35 @@ const BattlefieldPage = (props) => {
             <Sidemenu handleQuit={props.handleQuit} />
             <div className='battlefield_menu'>
                 <div className='tab-container'>
-                    <a href="/" className='menu-tab'>
+                    <Link href="/" className='menu-tab'>
                         <div>Home</div>
-                    </a>
-                    <a href="/" className='menu-tab'>
+                    </Link>
+                    <Link href="/" className='menu-tab'>
                         <div>Multiplayer</div>
-                    </a>
-                    <a href="/" className='menu-tab'>
+                    </Link>
+                    <Link href="/" className='menu-tab'>
                         <div>Campaign</div>
-                    </a>
-                    <a href="/" className={`menu-tab ${ navMenuIdx === 0 ? 'w--current' : ' '}`}
+                    </Link>
+                    <Link href="/" className={`menu-tab ${navMenuIdx === 0 ? 'w--current' : ' '}`}
                         onClick={() => setNavMenuIdx(0)}
                     >
                         <div>Soldier</div>
-                    </a>
-                    <a href="/" className={`menu-tab ${ navMenuIdx === 1 ? 'w--current' : ' '}`}
-                        onClick={() => setNavMenuIdx(1)}    
+                    </Link>
+                    <Link href="/" className={`menu-tab ${navMenuIdx === 1 ? 'w--current' : ' '}`}
+                        onClick={() => setNavMenuIdx(1)}
                     >
                         <div>Store</div>
-                    </a>
-                    <a href="/" className='menu-tab'>
+                    </Link>
+                    <Link href="/" className='menu-tab'>
                         <div>More</div>
-                    </a>
+                    </Link>
                 </div>
                 <div className='menu-bar' />
 
                 {navMenuIdx === 0 ?
-                    <div className='b4-content'>
-                        <div className='soldier__content'>
-                            <div className="soldier__look">
-                                <div className='soldier__dogtag'>
-                                    <img
-                                        src="https://uploads-ssl.webflow.com/6013fff62154adaa4600f932/6038fcc400244d4c8d9870cd_premium%20dog%20tag.png"
-                                        className='soldier__dogtag-left'
-                                    />
-                                    <img
-                                        src="https://uploads-ssl.webflow.com/6013fff62154adaa4600f932/6038fcc4c0c4fca91b669847_BF4_Celebration_Dog_Tag.png"
-                                        className='soldier__dogtag-right'
-                                    />
-                                </div>
-                                <img
-                                    src="https://uploads-ssl.webflow.com/6013fff62154adaa4600f932/6038fcc5c0c4fc008966984e_soldier__pic.png"
-                                    className='soldier__pic'
-                                />
-                            </div>
-
-                            <SoldierContent />
-                        </div>
-                    </div>
-                    :  
+                    <BattleFieldSoldier />
+                    :
                     <BattleFieldStore />
-
                 }
 
             </div>
